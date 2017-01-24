6c057b5be9df9e08ad89c0df78ab412e126ec33f
sagar.bhattachary@geminisolutions.in <sagar.bhattachary@geminisolutions.in>
2017-01-24 11:25:30 +0530

    maggi-datepicker directive fixed

--- js/shared/directives/datepicker.directive.js
+++ js/shared/directives/datepicker.directive.js
@@ -1,14 +1,28 @@
-angular.module('maggi.datepicker',[]).directive('mobi-datepicker',function(){
-    debugger;
+/*Datepicker Directive For Ios-Styled Datepicker*/
+angular.module('maggi.datepicker',[]).directive('mobiDatepicker',function(){
     return {
         restrict:'A',
         scope:{
-            'option':'=',
-            'theme':'='
+           options:'=options'
         },
-        link:function(scope,element,attrs){
-            element.val('').scroller('destroy').scroller($.extend(attrs.date,attrs.theme));
+        link:function(scope,element,attrs){  
+            var options = {
+                dateOption:{
+                    preset:'date',
+                    minDate: new Date(1900, 1, 1, 0,0,0),
+                    maxDate: new Date()
+                },
+                themeSet:{
+                    theme:'android-ics light',
+                    mode: 'scroller',
+                    display:'bottom', 
+                    lang:''
+                }
+            }
+            angular.extend(options.dateOption, scope.options.dateOption);
+            angular.extend(options.themeSet, scope.options.themeSet);
+            $(element).val('').scroller('destroy').scroller($.extend(options.dateOption,options.themeSet));
         }
     };
 
-})
+});
--- templates/login/login.controller.js
+++ templates/login/login.controller.js
@@ -2,16 +2,14 @@ angular.module('maggi').controller('LoginCtrl',loginController);
 
 function loginController ($http,$scope,$state,$rootScope,$location,$timeout){ 
 
-  
-    $scope.dateOption = {
-    preset:'date',
-    minDate:new Date(1970, 1, 1, 0,0,0),
-    maxDate:new Date(2014,12,12,0,0,0)
-    };
-    $scope.dateTheme = {
-        theme:'android-ics light',
-        mode: 'mixed',
-        display:'modal', 
-        lang:''
+   /*If no option provided then it will default to directory defaults*/
+   $scope.dateOpt = {
+        dateOption:{
+            minDate:new Date(1970, 1, 1, 0,0,0),
+            maxDate:new Date(2014,12,12,0,0,0)
+        }
     }
+
+  
+   
 };
